f2f2fd73388d2f3b8a27945c355d6f10ec1b0a2f
milanpatel41234 <mpatel41234@gmail.com>
2023-11-11 12:09:21 +0530

    drop down added for searching

--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -3,6 +3,9 @@ import style from "./HomePage.module.css";
 
 function HomePage() {
   const [ListItems, setListItems] = useState([]);
+  const [Language , setLanguage] = useState('kannada');
+
+  
   async function fetchMovieList() {
     const apiUrl = "https://hoblist.com/api/movieList";
     const requestData = {
@@ -12,43 +15,76 @@ function HomePage() {
       },
       body: JSON.stringify({
         category: "movies",
-        language: "kannada",
+        language: Language,
         genre: "all",
         sort: "voting",
       }),
     };
-  
+    
     try {
       const response = await fetch(apiUrl, requestData);
       const data = await response.json();
-      console.log(data.result)
-      setListItems(data.result)
+      console.log(data.result);
+      setListItems(data.result);
     } catch (error) {
       console.error("Error fetching movie list:", error);
     }
   }
   
   useEffect(() => {
-    fetchMovieList()
+    fetchMovieList();
   }, []);
 
-  const MoviesList =  ListItems.map((movie , index)=>{
-    return <li key={index}>
-        <div  className={style.card} >
-        <img src={movie.poster} alt={movie.name} />
-        <div>
-         <h4>{movie.title}</h4>
-         <p><b>Genre : </b>{movie.genre}</p>
-         <p><b>Director : </b>{movie.director[0]}</p>
-         <p><b>Starring : </b>{movie.stars[0]}</p>
-         <p><b>Language : </b>{movie.language}</p>
-        </div>
+  const HandleSearch = ()=>{
+  fetchMovieList();
+  }
+  
+  const MoviesList = ListItems.map((movie, index) => {
+    return (
+      <li key={index}>
+        <div className={style.card}>
+          <img src={movie.poster} alt={movie.name} />
+          <div>
+            <h4>{movie.title}</h4>
+            <p>
+              <b>Genre : </b>
+              {movie.genre}
+            </p>
+            <p>
+              <b>Director : </b>
+              {movie.director[0]}
+            </p>
+            <p>
+              <b>Starring : </b>
+              {movie.stars[0]}
+            </p>
+            <p>
+              <b>Language : </b>
+              {movie.language}
+            </p>
+          </div>
         </div>
         <button>Watch Trailer</button>
-    </li>
-  })
+      </li>
+    );
+  });
 
-  return <div className={style.container}>{MoviesList}</div>;
+  return (
+    <main className={style.main}>
+      <div>
+     <select placeholder="Search by language" value={Language} onChange={e=> setLanguage(e.target.value)}>
+      <option value='kannada'>kannada</option>
+      <option value='hindi'>Hindi</option>
+      <option value='english'>English</option>
+      <option value='tamil'>Tamil</option>
+      <option value='malayalam'>Malayalam</option>
+      <option value='all'>All</option>
+     </select>
+     <button onClick={HandleSearch} >Search</button>
+     </div>
+      <div className={style.container}>{MoviesList}</div>
+    </main>
+  );
 }
 
 export default HomePage;
